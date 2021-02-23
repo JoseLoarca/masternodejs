fd955094601da8c5686fac109c3ca63552a04988
José Loarca <joseloarca97@icloud.com>
2021-02-22 21:28:39 -0600

    implement delete product functionality

--- models/product.js
+++ models/product.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+const Cart = require('./cart');
 
 const getProductsFromFile = (callback) => {
     fs.readFile(p, ((err, fileContent) => {
@@ -29,6 +30,18 @@ module.exports = class Product {
         getProductsFromFile(products => {
             const product = products.find(p => p.id === id);
             callback(product);
+        });
+    }
+
+    static deleteById(id) {
+        getProductsFromFile(products => {
+            const product = products.find(prod => prod.id === id);
+            const updatedProducts = products.filter(prod => prod.id !== id);
+            fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+                if (!err) {
+                    Cart.deleteProduct(id, product.price);
+                }
+            });
         });
     }
 

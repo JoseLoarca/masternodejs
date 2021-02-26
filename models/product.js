5e8799d2f09f18d1bb27136dd739913a1e62b68b
José Loarca <joseloarca97@icloud.com>
2021-02-25 22:54:37 -0600

    implement raw sql queries execution

--- models/product.js
+++ models/product.js
@@ -1,17 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-const Cart = require('./cart');
-
-const getProductsFromFile = (callback) => {
-    fs.readFile(p, ((err, fileContent) => {
-        if (err) {
-            callback([]);
-        } else {
-            callback(JSON.parse(fileContent));
-        }
-    }));
-}
+// db
+const db = require('../util/database');
 
 module.exports = class Product {
     constructor(id, title, imageUrl, price, description) {
@@ -22,45 +10,20 @@ module.exports = class Product {
         this.price = price;
     }
 
-    static fetchAll(callback) {
-        getProductsFromFile(callback)
+    static fetchAll() {
+        return db.execute('SELECT * FROM products');
     }
 
-    static getById(id, callback) {
-        getProductsFromFile(products => {
-            const product = products.find(p => p.id === id);
-            callback(product);
-        });
+    static getById(id) {
+        return db.execute('SELECT * FROM products WHERE id = ?', [id]);
     }
 
     static deleteById(id) {
-        getProductsFromFile(products => {
-            const product = products.find(prod => prod.id === id);
-            const updatedProducts = products.filter(prod => prod.id !== id);
-            fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-                if (!err) {
-                    Cart.deleteProduct(id, product.price);
-                }
-            });
-        });
+
     }
 
     save() {
-        getProductsFromFile(products => {
-            if (this.id) {
-                const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-                const updatedProducts = [...products];
-                updatedProducts[existingProductIndex] = this;
-                fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-                    console.log(err);
-                });
-            } else {
-                this.id = Math.random().toString();
-                products.push(this);
-                fs.writeFile(p, JSON.stringify(products), (err) => {
-                    console.log(err);
-                });
-            }
-        });
+        return db.execute('INSERT INTO products(title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+            [this.title, this.price, this.imageUrl, this.description]);
     }
 }

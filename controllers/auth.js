31cab1a3d0f0899a1a3a6c992fae10062b3955a1
José Loarca <joseloarca97@icloud.com>
2021-07-16 22:35:46 -0600

    signup skeleton

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,31 +1,39 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-    console.log(req.session);
-    console.log(req.session.isLoggedIn);
-    res.render('auth/login', {
-        path: '/login',
-        pageTitle: 'Login',
-        isAuthenticated: false
-    });
-}
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
 
 exports.postLogin = (req, res, next) => {
-    User.findById('60ed156d3bb1d02760d73603')
-        .then(user => {
-            req.session.user = user;
-            req.session.isLoggedIn = true;
-            req.session.save((err) => {
-                console.log(err);
-                res.redirect('/');
-            });
-        })
-        .catch(err => console.log(err));
-}
-
-exports.postLogout = (req, res, next) => {
-    req.session.destroy(err => {
+  User.findById('60ed156d3bb1d02760d73603')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
         console.log(err);
         res.redirect('/');
-    });
-}
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};

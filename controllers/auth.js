4a40e1fd8bab3f08968693ba0d42a5e8271b76ec
José Loarca <joseloarca97@icloud.com>
2021-07-18 00:22:08 -0600

    implement nodemailer

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+    auth: {
+        api_key: process.env.SENDGRID_KEY
+    }
+}))
 
 exports.getLogin = (req, res, next) => {
     let message = req.flash('error');
@@ -88,6 +96,13 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
             res.redirect('/login');
+
+            return transporter.sendMail({
+               to: email,
+               from: process.env.SENGRID_FROM_ADDRESS,
+               subject: 'Signup succeeded!',
+               html: '<h1>You successfully signed up!</h1>'
+            });
         })
         .catch(err => {
             console.log(err);

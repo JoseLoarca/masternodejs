7a4442c9b2b7d2d51f949dcfc6e2ff859869fac5
José Loarca <joseloarca97@icloud.com>
2021-07-18 12:58:21 -0600

    implement auth validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,10 @@
 const express = require('express');
+
+const {check, body} = require('express-validator/check');
+
 const authController = require('../controllers/auth');
+
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -7,9 +12,44 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post('/login', [
+    body('email')
+        .isEmail()
+        .withMessage('Please enter a valid email.')
+        .normalizeEmail(),
+    body('password', 'Password must have at least 6 characters and be alphanumeric.')
+        .isLength({min: 6})
+        .isAlphanumeric()
+        .trim()], authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post('/signup', [
+    check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email.')
+        .custom((value, {req}) => {
+            return User
+                .findOne({email: value})
+                .then(userDoc => {
+                    if (userDoc) {
+                        return Promise.reject('Email exists already!');
+                    }
+                });
+        })
+        .normalizeEmail(),
+    body('password', 'Password must have at least 6 characters and be aphanumeric')
+        .isLength({min: 6})
+        .isAlphanumeric()
+        .trim(),
+    body('confirmPassword')
+        .custom((value, {req}) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords have to match!');
+            }
+
+            return true;
+        })
+        .trim()
+    ], authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
